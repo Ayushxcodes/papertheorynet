e087b615f66bc3588771ba4f5c35155387b4c3e6
Ayushxcodes <krayashford989@gmail.com>
2025-11-20 01:21:56 +0530

    again_change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import CircularGallerySection from "@/components/Landing/CircularGallerySection";
 import SimpleHero from "@/components/Landing/HeroSection";
 import ServicesCardSwap from "@/components/Landing/PolicySection";
@@ -6,12 +6,15 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div className="relative w-full h-full bg-black opacity-90">
-      {/* 🔥 Full-Page Plasma Background */}
-      <div className="fixed inset-0 -z-10">
-        <img src='./background.jpg' />
-      </div>
-
+    <div
+      className="relative w-full min-h-screen bg-black bg-opacity-90"
+      style={{
+        backgroundImage: "url('./background.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       {/* 🔻 Page Content */}
       <SimpleHero />
       <CircularGallerySection />
--- src/components/Base/ClientLayoutWrapper.tsx
+++ src/components/Base/ClientLayoutWrapper.tsx
@@ -3,14 +3,21 @@
 import React from "react";
 import NavApp from "@/components/Base/Navbar";
 
-export default function ClientLayoutWrapper({ children }: { children: React.ReactNode }) {
+export default function ClientLayoutWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="relative">
-      {/* 🔥 Global Plasma Background */}
-      <div className="fixed inset-0 -z-10">
-        
-      </div>
-
+    <div
+      className="relative min-h-screen"
+      style={{
+        backgroundImage: "url('./background.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       {/* 🔥 Global Navbar */}
       <div className="relative z-20">
         <NavApp />
